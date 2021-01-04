e67fb1e9ad5aba489d88bd4e4475bbcafa17305a
jordi.manyer <jordi.manyer@upc.edu>
2021-01-04 19:15:29 +0100

    Added restart to GMRES and tested algo.
    GMRES now has restart scheme for big problems.
    Algorithm tested for symmetric matrices and
    non-symmetric matrices.

--- src/Tests/v0_testGMRES.cpp
+++ src/Tests/v0_testGMRES.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <cmath>
 #include "sparse_LIL.h"
 #include "sparse_CSR.h"
 #include "solver_GMRES.h"
@@ -10,7 +11,7 @@ using namespace upa;
 
 
 int main() {
-    int n = 3;
+    int n = 10;
     Sparse_LIL lil(n);
 
     ///  Assembling the matrix in LIL format
@@ -19,6 +20,8 @@ int main() {
         if (i != 0  ) lil.put(i,i-1,-1);
         if (i != n-1) lil.put(i,i+1,-1);
     }
+
+    lil.put(0,2,3); // Break symmetry
 
     /// Convert to CSR
     auto csr = Sparse_CSR(&lil);
@@ -33,12 +36,15 @@ int main() {
 
     // System declaration
     double b[n], x[n], x0[n], r[n];
-    b[0] = 1.0; b[1] = 2.0; b[2] = 3.0;
-    x0[0] = 1.0; x0[1] = 1.0; x0[2] = 1.0;
+    for (int i = 0; i < n; ++i) {
+        b[i] = i;
+        x0[i] = 1.0;
+    }
 
     // Solve system
     Solver_GMRES GMRES(n, &csr, b);
     GMRES.setTolerance(0.000001);
+    GMRES.setRestart(true, 5);
     GMRES.setVerbosity(1);
     GMRES.solve(x0);
 
@@ -53,4 +59,14 @@ int main() {
     cout << "x = " << endl;
     printArray(n,x);
 
+    // Check that the residual is well calculated
+    double err = 0.0;
+    csr.matvec(x,r);
+    for (int i = 0; i < n; ++i) {
+        r[i] -= b[i];
+        err += r[i]*r[i];
+    }
+    err = sqrt(err);
+    cout << "Residual = " << err << endl;
+    printArray(n,r);
 }
